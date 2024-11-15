bfdc3c9aca1754d567f0df19b3a64e0fd50fe1cb
Eduardo Esplinio <eduesplinio.dat@gmail.com>
2024-11-15 16:14:08 -0300

    atualiza estilos e scripts

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadHTML('header', 'header.html');
+    loadHTML('footer', 'footer.html');
+});
+
+function loadHead(filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.head.innerHTML += data;
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadHead('head.html');
+});
--- scripts.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Adicione aqui as funcionalidades JavaScript necessárias
-});
